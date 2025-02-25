16a3457bb4e682e92c54781b98420dce437d80ce
Junsub Lee <lee980605@knu.ac.kr>
2025-02-25 21:39:42 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ring-us/sw.js', { scope: '/ring-us/' })})}
